e6b2f93b9ecfece3bd20a49791b976dedc29d3cc
Muhd Rafiq <109337275+mraf881@users.noreply.github.com>
2022-09-12 20:28:50 +0800

    Update first_try_with_pandas.py

--- first_try_with_pandas.py
+++ first_try_with_pandas.py
@@ -6,3 +6,22 @@ df=pd.read_csv("location.csv") #read the csv files
 df.head() #show the table
 
 df.column_1 #show the series of value available at the selected column
+
+df.head(10) # to display only 10 row
+
+df.columns # to show column in the dataframe
+
+df.total_rooms[0] #display the value for assigned position in [] according defined column
+
+df.iloc[0,0] #select value using index based selection
+
+df.loc[0:5,["total_rooms","total_bedrooms"]] #selectiing the six row of defined column df.loc[<initial num row>:<final num row>,["column_name1", "column_name2"]]
+
+#please note the return value of row of iloc and loc is not the same
+
+df.describe() #give quick summary of the table
+
+df.info() #another quick overview with different perspective
+
+df.shape # return the (value of row, column)
+
