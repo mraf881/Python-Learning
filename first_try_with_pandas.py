60b7d2db26725a4aa7d6cd78c15feff2afee2592
Muhd Rafiq <109337275+mraf881@users.noreply.github.com>
2022-09-12 19:15:50 +0800

    Update first_try_with_pandas.py

--- first_try_with_pandas.py
+++ first_try_with_pandas.py
@@ -4,3 +4,5 @@ import pandas as pd #importing the pandas
 df=pd.read_csv("location.csv") #read the csv files
 
 df.head() #show the table
+
+df.column_1 #show the series of value available at the selected column
