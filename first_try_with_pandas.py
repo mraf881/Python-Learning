a8f4b0bdc4f5f9943b262b739598734979ba647a
Muhd Rafiq <109337275+mraf881@users.noreply.github.com>
2022-09-15 19:19:40 +0800

    Update first_try_with_pandas.py

--- first_try_with_pandas.py
+++ first_try_with_pandas.py
@@ -25,3 +25,16 @@ df.info() #another quick overview with different perspective
 
 df.shape # return the (value of row, column)
 
+df.column1.max # return max value of selected column
+
+df.column1.min # return min value of selected column
+
+df.column1 == 'value' # conditional selection
+
+df[df.column1 == 'value'] # it show the result in form of table
+
+df.loc[(df.column1 == 'value') | (df.column1 == 'value2')]
+
+df.[df.column1.isin(["value1","value2"])
+
+
