42232797f1bef1812f28d4ba5774e8b1378adc2a
Muhd Rafiq <109337275+mraf881@users.noreply.github.com>
2022-09-16 20:30:04 +0800

    Update Pandas-Data Wrangling.py

--- Pandas-Data Wrangling.py
+++ Pandas-Data Wrangling.py
@@ -1,3 +1,5 @@
+#Renaming and dropping column
+
 df.rename(column={"column1":"column_id"}) #renaming the column1 to column_id
 
 df.rename(column={"column1":"column_id","column2":"column_name"}) #to rename multiple column
@@ -6,9 +8,13 @@ df.rename(column={"column1":"column_id","column2":"column_name"}, inplace=True) 
 
 df.drop(["column1","column2"], axis=1, inplace=True) #drop multiple column
 
+#Duplicate
+
 df.duplicate().sum() #to show the duplicate.
 
 df.drop_duplicate(inplac=True) #To remove the duplicate
+
+#To check and remove missing value.
 
 df.isna() #check missing value. True is the missing value.
 
@@ -27,4 +33,15 @@ df.column1.fillna(0,inplace=True) #to show the changes in the next dataframe.
 df.["New_Column1"]=0 #Create new column with 0 value in it.
 
 df.["is missing"] = df.column1.isna() #create new column with name "is missing" and show the null value is present acording to column1 as value.
+
+#To Change Data Type
+
+df.column1.dtype #To show data type for column1.
+
+df.column1.astype("object") #Change the data type to object.
+
+
+
+
+
 
