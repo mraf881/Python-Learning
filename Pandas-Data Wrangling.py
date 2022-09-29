5af74d789cc09e1ab1dec0dc3347531750eacf21
Muhd Rafiq <109337275+mraf881@users.noreply.github.com>
2022-09-29 09:58:45 +0800

    Update Pandas-Data Wrangling.py

--- Pandas-Data Wrangling.py
+++ Pandas-Data Wrangling.py
@@ -1,10 +1,10 @@
 #Renaming and dropping column
 
-df.rename(column={"column1":"column_id"}) #renaming the column1 to column_id
+df.rename(columns={"column1":"column_id"}) #renaming the column1 to column_id
 
-df.rename(column={"column1":"column_id","column2":"column_name"}) #to rename multiple column
+df.rename(columns={"column1":"column_id","column2":"column_name"}) #to rename multiple column
 
-df.rename(column={"column1":"column_id","column2":"column_name"}, inplace=True) #put inplace=True to show the changes made in the next dataframe.
+df.rename(columns={"column1":"column_id","column2":"column_name"}, inplace=True) #put inplace=True to show the changes made in the next dataframe.
 
 df.drop(["column1","column2"], axis=1, inplace=True) #drop multiple column
 
